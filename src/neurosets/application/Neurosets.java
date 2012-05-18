360e10c9ea86620937b09edf86ff0f801b3c6a9a
parsah <phossein@gmu.edu>
2012-05-18 12:11:31 -0400

    Revised gitignore and created skeleton classes to parse CSV files

---
+++ src/neurosets/application/Neurosets.java
@@ -0,0 +1,11 @@
+package neurosets.application;
+
+// Represents the main application class; drives analysis
+class Neurosets {
+	
+	// Initiate analysis
+	public static void main(String args[]) {
+		
+	}
+
+}
---
+++ src/neurosets/parse/CSVParser.java
@@ -0,0 +1,17 @@
+package neurosets.parse;
+
+/**
+ * Contains functions to parse CSV files containing neurite information.
+ * To fully execute dataset parsing, two CSV files are required: one must have
+ * metadata while the other must have neurite branch-lengths given a motif
+ * 
+ * @author Parsa Hosseini
+ * 
+ * */
+public class CSVParser {
+	
+	public void parseMetadata() {
+		
+	}
+
+}
